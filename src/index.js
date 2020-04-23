a9ff3dd6dbb380dcc0a2fa10f4c3eeca6febfffb
Raymond Johannessen <rajohan1@gmail.com>
2020-04-23 03:50:01 +0200

    The game is done and ready to be published

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import { Socket } from "react-socket-io";
 import App from "./components/App";
 
 const Root = () => {
+    const uri = process.env.NODE_ENV === "production" ? "/" : "http://localhost:8080";
+
     return (
         <BrowserRouter>
-            <Socket uri={"http://localhost:8080"} options={{ transports: ["websocket"] }}>
+            <Socket uri={uri} options={{ transports: ["websocket"] }}>
                 <App />
             </Socket>
         </BrowserRouter>
